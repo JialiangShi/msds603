9cd594bb036aaec7934bb890aa70ceb3f1e3e7ae
Jialiang Shi <jialiang.shi@jialiang.local>
2019-03-26 18:08:25 -0700

    with new API key

--- 2019-msds603-example/Week1/driving_time_example/calculate_driving_time.py
+++ 2019-msds603-example/Week1/driving_time_example/calculate_driving_time.py
@@ -6,7 +6,7 @@ from user_definition import *
 
 output_file = open(output_file_name, "a")
 # https://developers.google.com/maps/documentation/javascript/get-api-key
-apikey = 'AIzaSyCTWPyWsLgoMH6w_1iEf161paxhTiK1dNk'
+apikey = 'AIzaSyCkHWOkKuAY-iUcPgJM87r4S8ydImeTro8'
 url = "https://maps.googleapis.com/maps/api/distancematrix/json?key={0}&origins={1}&destinations={2}&mode=driving&departure_time=now&language=en-EN&sensor=false".format(
     str(apikey), str(orig_coord), str(dest_coord))
 result = simplejson.load(urllib.request.urlopen(url))
